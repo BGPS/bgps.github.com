de786c34540f0000a004b5809bffe653b4ae6dea
Adam Ginsburg <keflavich@gmail.com>
2013-01-15 12:07:40 -0700

    removed unnecessary stuff (but I still have a weird mix of github pages and pelican)

---
+++ publishconf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import sys
+sys.path.append('.')
+from pelicanconf import *
+
+SITEURL = ''
+
+DELETE_OUTPUT_DIRECTORY = True
+
+FEED_DOMAIN="http://bgps.github.com/"
+
+# Following items are often useful when publishing
+
+# Uncomment following line for absolute URLs in production:
+#RELATIVE_URLS = False
+
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
